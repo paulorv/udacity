b3b5252c5d7d76d284427618a2f2cfa3aa820a98
Paul Orvin <paulorvin@gmail.com>
2012-06-06 16:06:56 -0700

    coursework

--- CS101/exp-3.1.py
+++ CS101/exp-3.1.py
@@ -24,38 +24,53 @@ def proc2(mylist):
 # Can you explain the results given by the four print statements below? Remove
 # the hashes # and run the code to check.
 
-#print list1
-#proc(list1)
-#print list1
+print list1
+proc(list1)
+print list1
 
-#print list2
-#proc2(list2)
-#print list2
+print list2
+proc2(list2)
+print list2
 
-# Python has a special assignment syntax: +=.  Here is an example:
+## Python has a special assignment syntax: +=.  Here is an example:
 
 list3 = [1,2,3,4]
 list3 += [5]
 
-# Does this behave like list1 = list1 + [5] or list2.append(5)? Write a
-# procedure, proc3 similar to proc and proc2, but for +=. When you've done
-# that check your conclusion using the print-procedure call-print code as
-# above.
+## Does this behave like list1 = list1 + [5] or list2.append(5)? Write a
+## procedure, proc3 similar to proc and proc2, but for +=. When you've done
+## that check your conclusion using the print-procedure call-print code as
+## above.
 
-# What happens when you try:
+def proc3(mylist):
+	mylist += [6]
+	
+	
+print list3
+proc3(list3)
+print list3
+
+## What happens when you try:
 
 list1 = list1 + [7,8,9]
 list2.append([7,8,9])
 list3 += [7,8,9]
 
-# When you've understood the difference between the three methods,
-# write a procedure list_test which takes three lists as inputs. You should
-# mutate the first input list to include 'a' as the last entry, mutate the
-# second to include the entries 'a', 'b', 'c' and finally the last list should
-# not be mutated but a copy should be returned with the additional entry 'w'.
+print list1
+print list2
+print list3
 
-def list_test():
-    # Your code here
+## When you've understood the difference between the three methods,
+## write a procedure list_test which takes three lists as inputs. You should
+## mutate the first input list to include 'a' as the last entry, mutate the
+## second to include the entries 'a', 'b', 'c' and finally the last list should
+## not be mutated but a copy should be returned with the additional entry 'w'.
+
+def list_test(a,b,c):
+	a.append('a')
+	b += ['a','b','c']
+	c = c + ['w']
+	return c
 
 
 
@@ -63,12 +78,12 @@ first_input = [1,2,3]
 second_input = [4,5,6]
 third_input = [7,8,9]
 
-#print list_test(first_input, second_input, third_input)
-#>>> [7,8,9,'w']
-#print first_input
-#>>> [1,2,3,'a']
-#print second_input
-#>>> [4,5,6,'a','b','c']
-#print third_input
-#>>> [7,8,9]
+print list_test(first_input, second_input, third_input)
+##>>> [7,8,9,'w']
+print first_input
+##>>> [1,2,3,'a']
+print second_input
+##>>> [4,5,6,'a','b','c']
+print third_input
+##>>> [7,8,9]
 
--- CS101/exp-3.2.py
+++ CS101/exp-3.2.py
@@ -2,33 +2,46 @@
 # the second row is the same as the second column and so on. Write a
 # procedure, symmetric, which takes a list as input, and returns the
 # boolean True if the list is symmetric and False if it is not.
-def symmetric():
-    # Your code here
-
-#print symmetric([[1, 2, 3],
-#                [2, 3, 4],
-#                [3, 4, 1]])
+def symmetric(p):
+     i = 0
+     while i < len(p):
+         j = 0
+         if len(p[i]) != len(p):
+             return False
+         for e in p[i]:
+             if e != p[j][i]:
+                 return False
+             j += 1
+         i += 1
+     return True
+		
+	
+print symmetric([[1, 2, 3],
+                [2, 3, 4],
+                [3, 4, 1]])
 #>>> True
 
-#print symmetric([["cat", "dog", "fish"],
-#                ["dog", "dog", "fish"],
-#                ["fish", "fish", "cat"]])
-#>>> True
-
-#print symmetric([["cat", "dog", "fish"],
-#                ["dog", "dog", "dog"],
-#                ["fish","fish","cat"]])
+print symmetric([[1, 2, 3, 4],
+                [2, 3, 4, 5],
+                [3, 4, 5, 6]])
 #>>> False
 
-#print symmetric([[1, 2],
-#                [2, 1]])
+print symmetric([["cat", "dog", "fish"],
+                ["dog", "dog", "fish"],
+                ["fish", "fish", "cat"]])
 #>>> True
 
-#print symmetric([[1, 2, 3, 4],
-#                [2, 3, 4, 5],
-#                [3, 4, 5, 6]])
+print symmetric([["cat", "dog", "fish"],
+                ["dog", "dog", "dog"],
+                ["fish","fish","cat"]])
 #>>> False
 
-#print symmetric([[1,2,3],
-#                 [2,3,1]])
+print symmetric([[1, 2],
+                [2, 1]])
+#>>> True
+
+
+
+print symmetric([[1,2,3],
+                 [2,3,1]])
 #>>> False
