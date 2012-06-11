a008e90e40992f6f7eaed2956855b84227f6fbba
Paul Orvin <paulorvin@gmail.com>
2012-06-10 22:41:38 -0700

    weekend coursework

--- CS101/hw-4.4.py
+++ CS101/hw-4.4.py
@@ -14,23 +14,44 @@
 # splitlist.
 
 
+# My method. Does not work with 2nd sample
 def split_string(source,splitlist):
 	keywords = []
-	separators = []
-	for i in splitlist:
-		separators.append(i)
+	pos = 0
+	lastpos = 0
+	for i in source:
+		pos += 1
+		if i in splitlist:
+			keywords.append(source[lastpos:pos-1])
+			lastpos = pos
+	return keywords
 
 
+# Peter's method
+def split_string2(source,splitlist):
+	output = []
+	atsplit = True #at a split point
+	for char in source: #iterate through string by each letter
+		if char in splitlist:
+			atsplit = True
+		else:
+			if atsplit:
+				output.append(char)
+				atsplit = False
+			else:
+				#add character to last word
+				output[-1] = output[-1] + char
+	return output
 
 
 out = split_string("This is a test-of the,string separation-code!"," ,!-")
 print out
 #>>> ['This', 'is', 'a', 'test', 'of', 'the', 'string', 'separation', 'code']
 
-#out = split_string("After  the flood   ...  all the colors came out.", " .")
-#print out
+out = split_string("After  the flood   ...  all the colors came out.", " .")
+print out
 #>>> ['After', 'the', 'flood', 'all', 'the', 'colors', 'came', 'out']
 
-#out = split_string("First Name,Last Name,Street Address,City,State,Zip Code",",")
-#print out
+out = split_string("First Name,Last Name,Street Address,City,State,Zip Code",",")
+print out
 #>>>['First Name', 'Last Name', 'Street Address', 'City', 'State', 'Zip Code']
